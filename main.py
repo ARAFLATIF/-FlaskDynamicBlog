4c47dd9ca991339b61cbe126b2639af5800b5763
Araf Ibne Latif <124219901+ARAFLATIF@users.noreply.github.com>
2024-08-13 17:47:25 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in posts:
+        if blog_post["id"] == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
